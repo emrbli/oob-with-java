81d9e36bcff0c5d3ac57bbdb33e362f18b7a452e
Kemal Emre Ballı <kemal.emre.balli@gmail.com>
2018-07-30 15:54:15 +0300

    Database Bağlantı Main

---
+++ Database Bağlantı.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication33;
+
+/**
+ *
+ * @author Kemal
+ */
+public class JavaApplication33 {
+
+   
+    public static void main(String[] args) {
+        dbconnect connect =new dbconnect();
+        connect.getData();
+        
+        
+        
+        
+        
+    }
+    
+}
